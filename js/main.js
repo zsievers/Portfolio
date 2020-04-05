fb9683d672e08141f5e07149f8f60372df4bed6a
Zach <zjsievers@gmail.com>
2020-04-05 10:56:06 -0400

    adding template

---
+++ js/main.js
@@ -0,0 +1,98 @@
+//////////////////////////////////////////////////////  //
+//    EVENTS ON DOCUMENT READY                          //
+//////////////////////////////////////////////////////  //
+
+$(document).ready(function () {
+    "use strict";
+
+    ////  PRELOADING
+	$(window).on('load', function(){
+        $(".preloader-wrap").fadeOut(500); 
+    })
+    
+
+    //// COLLAPSED MENU CLOSE ON CLICK
+    $(document).on('click', '.navbar-collapse.in', function (e) {
+        if ($(e.target).is('a')) {
+            $(this).collapse('hide');
+        }
+    });
+
+    //// FIXED NAVBAR
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $('.navbar').addClass('fixed');
+        } else {
+            $('.navbar').removeClass('fixed');
+        }
+    });
+
+    /// Typed JS TRIGGER
+	$(".s-header .middle-l h3 span").typed({
+		strings: ["Web Designer", "UI/UX  Developer"],
+		loop: true,
+		startDelay: 1e3,
+		backDelay: 3e3
+	});
+
+	//// COUNT TO TRIGGER
+	var eventFired = false,
+        objectPositionTop = $('.facts').offset().top;
+    $(window).on('scroll', function () {
+        var currentPosition = $(document).scrollTop() + 400;
+        if (currentPosition >= objectPositionTop && eventFired === false) {
+            eventFired = true;
+            $(".count").countTo({
+                speed: 5000,
+                refreshInterval: 80
+            });
+        }
+    });
+
+	//// ISOTOPE TRIGGER
+	var $grid = $('.portfolio-content').isotope({
+	  itemSelector: '.portfolio-item',
+	  stagger: 30
+	});
+	$('.filter-portfolio').on( 'click', '.button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
+	// change is-checked class on buttons
+	$('.button-group').each( function( i, buttonGroup ) {
+	  var $buttonGroup = $( buttonGroup );
+	  $buttonGroup.on( 'click', 'a', function() {
+	    $buttonGroup.find('.is-checked').removeClass('is-checked');
+	    $( this ).addClass('is-checked');
+	  });
+	});
+
+	//// MASONRY
+	$('.portfolio-content').isotope({
+	  itemSelector: '.portfolio-caption img',
+	  masonry: {
+	    columnWidth: 0
+	  }
+	});
+  
+    //// MAGNIFIC POPUP TRIGGER
+	$('.ss-pic').magnificPopup({
+	  type: 'image'
+	});
+
+    //// PARSLEY TRIGGER
+	$('.contact-forum').parsley();
+	
+    //// SCROLL SPY TRIGGER
+ 	$('body').scrollspy({
+            target: '.navbar-collapse',
+            offset: 195
+    });
+
+   	//// SMOTH SCROLL
+    $.scrollIt({
+        topOffset: -80
+    });
+
+
+});
---
+++ php/contact.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+
+		<!-- Always force latest IE rendering engine -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<!-- TITLE -->
+		<title>PERSO - Contact</title>
+
+		<!-- BOOTSTRAP CSS -->
+		<link rel="stylesheet" href="../css/bootstrap/bootstrap.min.css">
+
+		<!-- FONT-AWESOME CSS -->
+		<link rel="stylesheet" href="../css/font-awesome/css/font-awesome.min.css">
+
+		<!-- MAIN STYLE CSS -->
+		<link rel="stylesheet" href="../css/style.css">
+
+		<!-- RESPONSIVE CSS -->
+		<link rel="stylesheet" href="../css/responsive.css">
+
+	</head>
+	<body class="contact-p">
+
+	<?php
+
+	$TO = 'you@youdomain.com';
+
+	//message the subject of the email
+	$SUBJECT = 'Contact from your WebSite';
+	$MSG_SEND_ERROR = 'Sorry, we can\'t send this message.';
+
+	// Sender Info
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+	$error = "";
+
+
+	// Email regex
+	$pattern = "^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$^";
+
+	// test for name
+	if (empty($name)) {
+	    $error .= 'error-name,'; // No name 	
+	}
+
+	// test for email
+	if (empty($email) || !preg_match_all($pattern, $email)) {
+	    $error .= 'error-email,'; // No Email	
+	}
+
+	// test for message
+	if (empty($message)) {
+	    $error .= 'error-message'; // No Message	
+	}
+
+
+	//define the headers we want passed. Note that they are separated with \r\n
+	$headers = "From: " . $name . " - " . $subject . " <" . $email . ">\r\nReply-To: " . $email . "";
+
+	if (!$error) {
+
+	    //send the email
+	    $send = mail($TO, $SUBJECT, $message, $headers);
+
+	    if ($send) {
+	    ?>
+		<div class="contact-f contact-success col-md-6">
+
+			<i class="fa fa-smile-o fa-4x"></i>
+
+			<h3>Thank You! Your message has been sent</h3>
+
+		</div>
+	    <?php
+	    } else {
+	        // If the message is not send return error
+	    ?>
+		<div class="contact-f contact-nosuccess col-md-6">
+
+			<i class="fa fa-frown-o fa-4x"></i>
+
+			<h3>Please complete the form and try again</h3>
+
+		</div>
+	<?php
+	    }
+	} else {
+	?>
+	<div class="contact-f contact-nosuccess col-md-6">
+
+		<i class="fa fa-frown-o fa-4x"></i>
+
+		<h3>Please complete the form and try again</h3>
+
+	</div>
+	<?php
+	}
+	?>
+
+		<!-- ====== JS ======  -->
+		<!-- JQUERY -->
+		<script type="text/javascript" src="../js/jquery.v1.12.4.js"></script>
+		<!-- BOOTSTRAP JS -->
+		<script src="../js/bootstrap.min.js"></script>
+		<!-- custom js -->
+		<script src="../js/main.js"></script>
+	</body>
+</html>
