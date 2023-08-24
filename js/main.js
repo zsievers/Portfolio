31b884d7a622a564aa9d938754a9192cd42de78e
Zach Sievers <zach.sievers@serrala.com>
2023-08-24 14:43:47 -0400

    edit js trigger content

--- js/main.js
+++ js/main.js
@@ -3,32 +3,37 @@
 //////////////////////////////////////////////////////  //
 
 $(document).ready(function () {
-  'use strict';
+  "use strict";
 
   ////  PRELOADING
-  $(window).on('load', function () {
-    $('.preloader-wrap').fadeOut(500);
+  $(window).on("load", function () {
+    $(".preloader-wrap").fadeOut(500);
   });
 
   //// COLLAPSED MENU CLOSE ON CLICK
-  $(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a')) {
-      $(this).collapse('hide');
+  $(document).on("click", ".navbar-collapse.in", function (e) {
+    if ($(e.target).is("a")) {
+      $(this).collapse("hide");
     }
   });
 
   //// FIXED NAVBAR
   $(window).scroll(function () {
     if ($(window).scrollTop() > 50) {
-      $('.navbar').addClass('fixed');
+      $(".navbar").addClass("fixed");
     } else {
-      $('.navbar').removeClass('fixed');
+      $(".navbar").removeClass("fixed");
     }
   });
 
   /// Typed JS TRIGGER
-  $('.s-header .middle-l h3 span').typed({
-    strings: ['Husband', 'Web Designer', 'Full Stack Developer', 'Dog Dad'],
+  $(".s-header .middle-l h3 span").typed({
+    strings: [
+      "Husband",
+      "Father",
+      "Software Developer",
+      "Future Cloud Engineer",
+    ],
     loop: true,
     startDelay: 1e3,
     backDelay: 3e3,
@@ -36,12 +41,12 @@ $(document).ready(function () {
 
   //// COUNT TO TRIGGER
   var eventFired = false,
-    objectPositionTop = $('.facts').offset().top;
-  $(window).on('scroll', function () {
+    objectPositionTop = $(".facts").offset().top;
+  $(window).on("scroll", function () {
     var currentPosition = $(document).scrollTop() + 400;
     if (currentPosition >= objectPositionTop && eventFired === false) {
       eventFired = true;
-      $('.count').countTo({
+      $(".count").countTo({
         speed: 5000,
         refreshInterval: 80,
       });
@@ -49,42 +54,42 @@ $(document).ready(function () {
   });
 
   //// ISOTOPE TRIGGER
-  var $grid = $('.portfolio-content').isotope({
-    itemSelector: '.portfolio-item',
+  var $grid = $(".portfolio-content").isotope({
+    itemSelector: ".portfolio-item",
     stagger: 30,
   });
-  $('.filter-portfolio').on('click', '.button', function () {
-    var filterValue = $(this).attr('data-filter');
+  $(".filter-portfolio").on("click", ".button", function () {
+    var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
   });
   // change is-checked class on buttons
-  $('.button-group').each(function (i, buttonGroup) {
+  $(".button-group").each(function (i, buttonGroup) {
     var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'a', function () {
-      $buttonGroup.find('.is-checked').removeClass('is-checked');
-      $(this).addClass('is-checked');
+    $buttonGroup.on("click", "a", function () {
+      $buttonGroup.find(".is-checked").removeClass("is-checked");
+      $(this).addClass("is-checked");
     });
   });
 
   //// MASONRY
-  $('.portfolio-content').isotope({
-    itemSelector: '.portfolio-caption img',
+  $(".portfolio-content").isotope({
+    itemSelector: ".portfolio-caption img",
     masonry: {
       columnWidth: 0,
     },
   });
 
   //// MAGNIFIC POPUP TRIGGER
-  $('.ss-pic').magnificPopup({
-    type: 'image',
+  $(".ss-pic").magnificPopup({
+    type: "image",
   });
 
   //// PARSLEY TRIGGER
-  $('.contact-forum').parsley();
+  $(".contact-forum").parsley();
 
   //// SCROLL SPY TRIGGER
-  $('body').scrollspy({
-    target: '.navbar-collapse',
+  $("body").scrollspy({
+    target: ".navbar-collapse",
     offset: 195,
   });
 
